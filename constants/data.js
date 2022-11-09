61f267e33744803463324aad3ac33ca23727b51d
gml3ff <morgan@lupton.io>
2022-11-09 14:54:13 -0500

    minor changes to wording

--- constants/data.js
+++ constants/data.js
@@ -54,46 +54,18 @@ const userData = {
     description: [
       `With a background in cloud operations engineering I have experience developing and deploying large scale applications with an emphasis on reliability and performance. I have carried these skills over to the sales side as well: using my knowledge of SRE principles to sell monitoring solutions to a wide variety of organizations ranging from eCommerce platforms, to Finance and Healthcare`,
       `Despite my experience in the software realm, I also have a penchant for product design in the physical world. I have taught myself 3D modeling using Fusion360 and have brought these models into reality using my 3D printer. Beyond the design of the models themselves, I will frequently construct projects that involve some kind of electronic hardware (e.g. Arduinos, Raspberry Pis, etc.). Everything from the soldering of wires and PCBs to the design of the software, I do it all. Sometimes with great success and sometimes with less success: but all of which provides for a valuable learning experience.`,
-      `Beyond these pursuits, I spend a great deal of my spare time DJ'ing and producing music. My wife and I host a monthly radio show on Eaton Radio here in D.C., and I play live around the area as well (mainly doing all-vinyl sets). I mainly make my own music within Ableton, but do also enjoy using hardware synthesizersocassionally (namely my Yamaha SY77 FM synthesizer).`,
+      `Beyond these pursuits, I spend a great deal of my spare time DJ'ing and producing music. My wife and I host a monthly radio show on Eaton Radio here in D.C., and I play live around the area as well (mainly doing all-vinyl sets). I make my own music within Ableton, but do also enjoy using hardware synthesizers on ocassion (namely my Yamaha SY77).`,
     ],
     currentProject: "Tailwind Master Kit",
     currentProjectUrl: "https://tailwindmasterkit.com",
   },
   experience: [
     {
-      title: "Software Developer",
-      company: "mroads",
-      year: "2021",
-      companyLink: "https://mroads.com",
-      desc: "Contributed to our flagship product - Panna - which is an AI powered candidate interviewing product.",
-    },
-    {
-      title: "Founder and Developer",
-      company: "TailwindMasterKit",
-      year: "2021",
-      companyLink: "https://tailwindmasterkit.com",
-      desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
-    },
-    {
-      title: "Founder and Developer",
-      company: "Algochurn",
-      year: "2021",
-      companyLink: "https://algochurn.com",
-      desc: "Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.",
-    },
-    {
-      title: "Internship",
-      company: "Intel Corporation",
-      year: "2019",
-      companyLink: "https://intel.com",
-      desc: "Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.",
-    },
-    {
       title: "Education",
       company: "University of Virginia.",
       year: "2015",
       companyLink: "https://virginia.edu",
-      desc: "Major in Computer Science, Minor in AStronomy",
+      desc: "Major in Computer Science, Minor in Astronomy",
     },
   ],
   resumeUrl:
