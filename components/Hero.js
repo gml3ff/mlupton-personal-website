8e7d5eeacbc64d7b9d9ce434540a3f10dd5b098c
gml3ff <gml3ff@virginia.edu>
2022-11-12 11:20:21 -0600

    add shading to image

--- components/Hero.js
+++ components/Hero.js
@@ -22,7 +22,7 @@ export default function Hero() {
       {/* Image container */}
       <div className="hidden lg:block relative w-full md:w-1/2 -mr-40 mt-20">
         <div className="w-1/2 ">
-          <img src={userData.avatarUrl} alt="avatar" className=" shadow" />
+          <img src={userData.avatarUrl} alt="avatar" className=" shadow" id="profPic"/>
           <div className="flex flex-row justify-between mt-4">
             <div className="flex flex-row space-x-4">
             </div>
