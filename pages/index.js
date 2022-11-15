73ec5851055b8fb0c25cc71e4a3119577fb29dba
gml3ff <gml3ff@virginia.edu>
2022-11-15 14:53:22 -0500

    remove latest code section from main page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import ContainerBlock from "../components/ContainerBlock";
-import LatestCode from "../components/LatestCode";
 import Hero from "../components/Hero";
-import getLatestRepos from "@lib/getLatestRepos";
 import userData from "@constants/data";
 
 export default function Home({ repositories }) {
@@ -17,16 +15,3 @@ export default function Home({ repositories }) {
   );
 }
 
-export const getServerSideProps = async () => {
-  console.log(process.env.GITHUB_AUTH_TOKEN);
-  let token = process.env.GITHUB_AUTH_TOKEN;
-
-  const repositories = await getLatestRepos(userData, token);
-  // console.log("REPOSITORIES", repositories);
-
-  return {
-    props: {
-      repositories,
-    },
-  };
-};
