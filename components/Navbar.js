478e4a2435424e7f57a375e64937f7f3a3e4a3de
gml3ff <gml3ff@virginia.edu>
2022-11-16 21:17:06 -0500

    remove music page, fix projects page

--- components/Navbar.js
+++ components/Navbar.js
@@ -115,33 +115,6 @@ export default function Navbar() {
               )}
             </a>
           </Link>
-          <Link href="/music">
-            <a
-              id="selection"
-              className={`text-2xl  ${
-                router.asPath === "/music"
-                  ? "text-gray-800 font-bold dark:text-gray-400"
-                  : "text-gray-600 dark:text-gray-300 font-normal "
-              }`}
-            >
-              Music{" "}
-              {router.asPath === "/music" && (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-arrow-down inline-block h-3 w-3"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
-                  />
-                </svg>
-              )}
-            </a>
-          </Link>
           <Link href="/contact">
             <a
               id="selection"
@@ -235,11 +208,6 @@ export default function Navbar() {
         <Link href="/projects">
           <a className="text-2xl font-normal text-gray-600 dark:text-gray-300">
             Projects
-          </a>
-        </Link>
-        <Link href="/music">
-          <a className="text-2xl font-normal text-gray-600 dark:text-gray-300">
-            Music
           </a>
         </Link>
         <Link href="/contact">
--- components/Projects.js
+++ components/Projects.js
@@ -21,47 +21,65 @@ export default function Projects() {
           Projects
         </h1>
       </div>
-    <div className="max-w-6xl mx-auto  py-2 md:py-4">
-      <div className="flex md:flex-row justify-between items-center">
-        <div className="space-x-12 hidden md:block">
-          <Link href="#one-knob">
-            <a
-              className="text-2xl text-[#004d4d] font-bold dark:text-gray-400 hover:border-4 rounded-sm p-2 border-[#008080]">
-              MIDI Control Knob{" "}
-            </a>
-          </Link>
-          <Link href="#vacuum-stand">
-            <a
-              className="text-2xl text-[#004d4d] font-bold dark:text-gray-400 hover:border-4 rounded-sm p-2 border-[#008080]">
-              Vacuum Stand{" "}
-            </a>
-          </Link>
-          <Link href="#streaming-remote">
-            <a
-              className="text-2xl text-[#004d4d] font-bold dark:text-gray-400 hover:border-4 rounded-sm p-2 border-[#008080]">
-              Streaming Remote
-            </a>
-          </Link>
-        </div>
+      <div className="max-w-6xl mx-auto  py-2 md:py-4">
+        <div className="flex md:flex-row justify-between items-center">
+          <div className="space-x-12 hidden md:block">
+            <Link href="#one-knob">
+              <a
+                className="text-2xl text-[#004d4d] font-bold dark:text-gray-400 hover:border-4 rounded-sm p-2 border-[#008080]">
+                MIDI Control Knob{" "}
+              </a>
+            </Link>
+            <Link href="#vacuum-stand">
+              <a
+                className="text-2xl text-[#004d4d] font-bold dark:text-gray-400 hover:border-4 rounded-sm p-2 border-[#008080]">
+                Vacuum Stand{" "}
+              </a>
+            </Link>
+            <Link href="#streaming-remote">
+              <a
+                className="text-2xl text-[#004d4d] font-bold dark:text-gray-400 hover:border-4 rounded-sm p-2 border-[#008080]">
+                Streaming Remote
+              </a>
+            </Link>
+          </div>
 
+        </div>
       </div>
-    </div>
       {/* Grid starts here */}
       <div className="bg-[#F1F1F1] dark:bg-gray-900">
-        <div className="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-16 py-20 pb-40">
-            <div className="grid grid-rows-1 md:grid-rows-2 ">
-            <h1 id="one-knob" className="text-3xl md:text-5xl align-baseline text-[#006666]">MIDI Control Knob</h1>
-              <p>A simple MIDI device for Ableton Live. Built using Max MSP and using an Arduino Nano as the controller. Automatically maps to any clicked parameters without requiring any sort of MIDI template. Great for quickly adjusting parameters like filter cutoff without requiring any sort of device template. </p> </div>
-              <video src="videos/KnobClip_2.mp4" autoplay="autoplay" muted loop id="videoFrame" />    
-            <div className="grid grid-rows-1 md:grid-rows-2">
-      <h1 id="vacuum-stand" className="text-3xl md:text-5xl text-[#006666]">Vacuum Stand</h1>
-              <p>Dissatisfied with the excessive cost of the charging stand for the handheld vacuum I own, I decided to design my own! This model can be 3D printed fairly easily and is <a href="https://www.thingiverse.com/thing:5341293">available for download on Thingiverse</a></p></div>
-              <img></img>
-          <h1 id="streaming-remote" className="text-3xl md:text-5xl text-[#006666]">Streaming Remote</h1>
-            <div className="grid grid-rows-2 md:grid-rows-6">
-              <p></p>
-              <img></img>
-            </div>
+        <div className="grid grid-cols-2 dark:bg-gray-900 max-w-6xl mx-auto pt-20 gap-x-16 gap-y-32">
+          <div className="max-w-4xl max-h-4xl"><h1 id="one-knob" className="text-3xl md:text-5xl align-baseline text-[#006666]">MIDI Control Knob</h1>
+          <br></br>
+          <br/>
+          <br/>
+          <p>A simple MIDI device for Ableton Live. Built using Max MSP and using an Arduino Nano as the controller. Automatically maps to any clicked parameters without requiring any sort of MIDI template. Great for quickly adjusting parameters like filter cutoff without requiring any sort of device template. </p></div>
+          <div className="max-w-4xl"><video src="videos/KnobClip_2.mp4" autoplay="autoplay" muted loop id="videoFrame" /></div>
+          
+          <img src="oneknob_diagram.png" id="projectImage" />
+          
+          <img src="knob_tall.jpg" id="projectImage" />
+          <div className="max-w-4xl">
+            <h1 id="vacuum-stand" className="text-3xl md:text-5xl text-[#006666]">Vacuum Stand</h1>
+            <br/>
+            <br/>
+            <br/>
+            <p>Dissatisfied with the excessive cost of the charging stand for the handheld vacuum I own, I decided to design my own! This model can be 3D printed fairly easily and is <a href="https://www.thingiverse.com/thing:5341293">available for download on Thingiverse</a></p>
+          </div>
+            
+          <img src="vacuumstand_diagram.png" id="projectImage" />
+          <img src="vacbottom.jpg" id="projectImage" />
+          <img src="vacstand.jpg" id="projectImage" />
+          <div className="max-w-4xl"> 
+            <h1 id="streaming-remote" className="text-3xl md:text-5xl text-[#006666]">Streaming Remote</h1>
+            <br></br>
+            <br/>
+            <br/>
+            <p>An exercise in rendering and more complex modeling techniques in Fusion 360. Taking design cues from the Laserdisc remotes of the early 90s, this streaming remote design also includes a jog wheel for frame-by-frame scrolling. The size and shape was iterated upon by 3D printing multiple prototypes.</p>
+          </div>
+          <img src="remote_diagram.png" id="projectImage" />
+          <img src="remote_hand.jpg" id="projectImage" />
+          <img src="remote_angled.png" id="projectImage" />
         </div>
       </div>
     </section>
